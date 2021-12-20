d2d538ec6b34a1653a603a1f1b58686b484c0470
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-12-20 21:08:35 +0100

    Create backend skeleton

---
+++ backend/api/src/main/java/com/github/projectbaka/home/api/HomeApplication.java
@@ -0,0 +1,13 @@
+package com.github.projectbaka.home.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeApplication.class, args);
+	}
+
+}
